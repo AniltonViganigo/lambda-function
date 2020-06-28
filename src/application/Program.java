13b396c3fa08661db5111d59c481b800c05af8ec
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-06-28 17:58:57 -0300

    Interface implementation

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import entities.Product;
+import util.UpperCaseName;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 250.0));
+		list.add(new Product("HD Case", 80.55));
+		
+		List<String> name = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		
+		name.forEach(System.out::println);
+
+		
+	}
+
+}
+
